f8403c2a20470c79ba237c6f0358d140429af6cd
pnarayanan31 <pnarayanan31@gatech.edu>
2020-06-26 16:27:25 -0400

    Create function tests

--- tests/com/martasim/datamgmt/SQLiteDatabaseTest.java
+++ tests/com/martasim/datamgmt/SQLiteDatabaseTest.java
@@ -37,6 +37,69 @@ class SQLiteDatabaseTest {
 
         assertEquals(1, routes.size());
         assertEquals(route, routes.get(0));
+
+        Stop stop = new Stop(0, "stop 0", 0, 0, 0);
+        List <Stop> stops = new ArrayList<>();
+        stops.add(stop);
+        Route route2 = new Route(1, 1, "route 1", stops);
+        db.addRoute(route2);
+
+        routes = db.getAllRoutes();
+
+        assertEquals(2, routes.size());
+        assertEquals(route2, routes.get(1));
+    }
+
+    @Test
+    void add_bus() throws SQLException {
+        List<Bus> busses = db.getAllBusses();
+        assertEquals(0, busses.size());
+
+        Route route = new Route(0, 0, "route 0");
+        Bus bus1 = new Bus(0, null, 0, 0 , 10 , 0, 10, 0);
+        Bus bus2 = new Bus(1, route, 1, 1 , 11 , 1, 11, 1);
+        db.addBus(bus1);
+
+        busses = db.getAllBusses();
+
+        assertEquals(1, busses.size());
+        assertEquals(bus1, busses.get(0));
+
+        db.addBus(bus2);
+        busses = db.getAllBusses();
+
+        assertEquals(2, busses.size());
+        assertEquals(bus2, busses.get(1));
+    }
+
+    @Test
+    void add_stop() throws SQLException {
+        List<Stop> stops = db.getAllStops();
+        assertEquals(0, stops.size());
+
+        Stop stop = new Stop (0, "stop 0", 0, 0, 0);
+
+        db.addStop(stop);
+
+        stops = db.getAllStops();
+
+        assertEquals(1, stops.size());
+        assertEquals(stop, stops.get(0));
+    }
+
+    @Test
+    void add_event() throws SQLException {
+        List<Event> events = db.getAllEvents();
+        assertEquals(0, events.size());
+
+        Event event = new Event (0, "not important" , 0);
+
+        db.addEvent(event);
+
+        events = db.getAllEvents();
+
+        assertEquals(1, events.size());
+        assertEquals(event, events.get(0));
     }
 
     @Test
