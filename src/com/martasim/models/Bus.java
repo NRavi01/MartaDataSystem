9a366df9ea1799fb2fb44957957c3ea80f84581b
Amaan Marfatia <amaanmarfatia@gmail.com>
2020-07-04 18:01:58 -0400

    Closes #31
    Updates the bus model to handle location as well as the current / last visitied stop

--- src/com/martasim/models/Bus.java
+++ src/com/martasim/models/Bus.java
@@ -5,17 +5,27 @@ import java.util.Objects;
 public class Bus {
     final int id;
     Route route;
-    int location;
+    int currentStop;
+    double latitude;
+    double longitude;
     int passengers;
     int passengerCapacity;
     double fuel;
     double fuelCapacity;
     double speed;
 
-    public Bus(int id, Route route, int location, int passengers, int passengerCapacity, double fuel, double fuelCapacity, double speed) {
+    public Bus(int id, Route route, double latitude, double longitude, int passengers,
+               int passengerCapacity, double fuel, double fuelCapacity, double speed) {
+        this(id, route, -1, latitude, longitude, passengers, passengerCapacity, fuel, fuelCapacity, speed);
+    }
+
+    public Bus(int id, Route route, int currentStop, double latitude, double longitude, int passengers,
+               int passengerCapacity, double fuel, double fuelCapacity, double speed) {
         this.id = id;
         this.route = route;
-        this.location = location;
+        this.setCurrentStopIndex(currentStop);
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.passengers = passengers;
         this.passengerCapacity = passengerCapacity;
         this.fuel = fuel;
@@ -28,7 +38,9 @@ public class Bus {
         return "(" +
                 id + ", " +
                 (route == null ? "null" : route.id) + ", " +
-                location + ", " +
+                currentStop + ", " +
+                latitude + ", " +
+                longitude + ", " +
                 passengers + ", " +
                 passengerCapacity + ", " +
                 fuel + ", " +
@@ -49,12 +61,70 @@ public class Bus {
         this.route = route;
     }
 
-    public int getLocation() {
-        return location;
+    public int getCurrentStopIndex() {
+        return currentStop;
     }
 
-    public void setLocation(int location) {
-        this.location = location;
+    public void setCurrentStopIndex(int currentStop) {
+        if (currentStop < -1) {
+            throw new RuntimeException("can't set current stop to any value less than -1");
+        } else if (currentStop != -1) {
+            if (route == null) {
+                throw new RuntimeException("can't set current stop to value other than -1 when the bus's route is null");
+            } else if (currentStop >= route.getStops().size()) {
+                throw new IndexOutOfBoundsException("can't set current stop to value greater than or equal to length of stops on bus's route");
+            }
+        }
+        this.currentStop = currentStop;
+    }
+
+    /**
+     * @return the stop the bus is at currently or the last stop the bus visited
+     */
+    public Stop getCurrentStop() {
+        if (currentStop == -1 || route == null) {
+            return null;
+        }
+        return route.getStops().get(currentStop);
+    }
+
+    public int getNextStopIndex() {
+        if (currentStop == -1) {
+            return -1;
+        }
+
+        return (currentStop + 1) % route.getStops().size();
+    }
+
+    public Stop getNextStop() {
+        if (getNextStopIndex() == -1) {
+            return null;
+        }
+
+        return route.getStops().get(getNextStopIndex());
+    }
+
+    /**
+     * Goes to the next stop if it exits and is possible
+     */
+    public void goToNextStop() {
+        currentStop = getNextStopIndex();
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
 
     public int getPassengers() {
@@ -103,7 +173,9 @@ public class Bus {
         if (!(o instanceof Bus)) return false;
         Bus bus = (Bus) o;
         return id == bus.id &&
-                location == bus.location &&
+                currentStop == bus.currentStop &&
+                Double.compare(bus.latitude, latitude) == 0 &&
+                Double.compare(bus.longitude, longitude) == 0 &&
                 passengers == bus.passengers &&
                 passengerCapacity == bus.passengerCapacity &&
                 Double.compare(bus.fuel, fuel) == 0 &&
@@ -114,6 +186,6 @@ public class Bus {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, route, location, passengers, passengerCapacity, fuel, fuelCapacity, speed);
+        return Objects.hash(id, route, currentStop, latitude, longitude, passengers, passengerCapacity, fuel, fuelCapacity, speed);
     }
 }
