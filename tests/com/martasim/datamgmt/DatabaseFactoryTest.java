797cf8a1122e79246e7bc120b4fde647ced64ed3
pnarayanan31 <pnarayanan31@gatech.edu>
2020-07-07 17:18:21 -0400

    Tests for DatabaseFactory's createDatabase functions (excluding GTFS)

---
+++ tests/com/martasim/datamgmt/DatabaseFactoryTest.java
@@ -0,0 +1,95 @@
+package com.martasim.datamgmt;
+
+import com.martasim.models.Event;
+import com.martasim.models.EventType;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class DatabaseFactoryTest {
+    @Test
+    void create_empty_database() throws SQLException {
+        File file = new File("MartaSimulation.db");
+        boolean deleted = true;
+        if (file.exists()) {
+            deleted = file.delete();
+        }
+
+        assertTrue(deleted);
+        assertTrue(!file.exists());
+
+        Database db = DatabaseFactory.createEmptyDatabase();
+
+        assertTrue(file.exists());
+        assertEquals(db.getAllBuses().size(), 0);
+        assertEquals(db.getAllRoutes().size(), 0);
+        assertEquals(db.getAllEvents().size(), 0);
+        assertEquals(db.getAllStops().size(), 0);
+
+        db.close();
+    }
+
+    @Test
+    void create_empty_database_with_file() throws SQLException {
+        File testfile = new File("TestFile.db");
+        File file = new File("MartaSimulation.db");
+        boolean deleted = true;
+        if (file.exists()) {
+            deleted = file.delete();
+        }
+
+        assertTrue(deleted);
+        assertFalse(file.exists());
+
+        deleted = true;
+        if (testfile.exists()) {
+            deleted = testfile.delete();
+        }
+
+        assertTrue(deleted);
+        assertFalse(testfile.exists());
+
+        Database db = DatabaseFactory.createEmptyDatabase(testfile);
+
+        assertTrue(testfile.exists());
+        assertEquals(db.getAllBuses().size(), 0);
+        assertEquals(db.getAllRoutes().size(), 0);
+        assertEquals(db.getAllEvents().size(), 0);
+        assertEquals(db.getAllStops().size(), 0);
+
+        db.close();
+    }
+
+    @Test
+    void create_database_with_file() throws SQLException {
+        File testfile = new File("TestFile.db");
+        boolean deleted = true;
+        if (testfile.exists()) {
+            deleted = testfile.delete();
+        }
+
+        assertTrue(deleted);
+        assertFalse(testfile.exists());
+
+        Database db1 = DatabaseFactory.createEmptyDatabase(testfile);
+
+        Event e = new Event(0, 0, EventType.move_bus);
+        db1.addEvent(e);
+
+        assertEquals(db1.getAllEvents().size(), 1);
+
+        db1.close();
+
+        Database db2 = DatabaseFactory.createDatabaseFromDb(testfile);
+
+        assertEquals(db2.getAllEvents().size(), 1);
+        assertTrue(db2.getAllEvents().contains(e));
+
+        db2.close();
+    }
+}
