8d091f2e94b1e7b558a3077a4849b604f692f143
Amaan Marfatia <amaanmarfatia@gmail.com>
2020-07-08 12:49:52 -0400

    Add basic fast parsing template

--- src/com/martasim/datamgmt/GtfsParser.java
+++ src/com/martasim/datamgmt/GtfsParser.java
@@ -1,5 +1,12 @@
 package com.martasim.datamgmt;
 
+import com.martasim.models.Route;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.util.zip.ZipFile;
 
 class GtfsParser extends Parser {
@@ -11,6 +18,28 @@ class GtfsParser extends Parser {
 
     @Override
     public void parse() {
-        // TODO (5/24/20): read the file, parse it, and add all information to the database
+        // TODO: read the zip file, parse it, and add all information to the database
+
+        try {
+            addRoutes(zipFile.getInputStream(zipFile.getEntry("routes.txt")));
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    private void addRoutes(InputStream inputStream) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        br.readLine();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line, ",");
+            
+            // TODO: Use st.next() to keep getting the next value in the csv
+            // Parse integers with Integer.parseInt(next())
+            // Parse doubles with Double.parseDouble(next())
+        }
+
+        br.close();
     }
 }
