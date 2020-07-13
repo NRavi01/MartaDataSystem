703ab9c590a615ec5ac27b6425fb0ffced3e42d4
wilwarner6 <wwarner7@gatech.edu>
2020-07-13 15:49:02 -0400

    updated tests

--- tests/com/martasim/datamgmt/ParserTest.java
+++ tests/com/martasim/datamgmt/ParserTest.java
@@ -12,16 +12,12 @@ import java.util.zip.ZipFile;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
-    static Database db;
-    static GtfsParser parser;
+    static private Database db;
 
     @BeforeAll
     static void parse_data() throws SQLException, IOException {
-        db = DatabaseFactory.createEmptyDatabase();
-
         ZipFile zip = new ZipFile("/Users/wilwarner6/vcs/wwarner7/github.gatech.edu/MartaSimDataMgmt/src/com/martasim/data/gtfs022118.zip");
-        parser = new GtfsParser(db, zip);
-        parser.parse();
+        db = DatabaseFactory.createDatabaseFromGtfs(zip);
     }
 
     @Test
